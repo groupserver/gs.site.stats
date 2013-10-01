91d93a336183cb6087cdd9dc47d21a8b7f63357b
Michael JasonSmith <mpj17@onlinegroups.net>
2013-10-01 15:17:13 -0700

    Initial import

---
+++ README.txt
@@ -0,0 +1,36 @@
+=================
+``gs.site.stats``
+=================
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Usage statistics for a GroupServer site
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Author: `Michael JasonSmith`_
+:Contact: Michael JasonSmith <mpj17@onlinegroups.net>
+:Date: 2013-10-01
+:Organization: `GroupServer.org`_
+:Copyright: This document is licensed under a
+  `Creative Commons Attribution-Share Alike 3.0 New Zealand License`_
+  by `OnlineGroups.Net`_.
+
+Introduction
+============
+
+This product provides a page providing the statistics for all groups in a
+GroupServer_ site. It relies on the group statistics product [#group]_.
+
+Resources
+=========
+
+- Code repository: https://source.iopen.net/groupserver/gs.site.stats/
+- Questions and comments to http://groupserver.org/groups/development/
+- Report bugs at https://redmine.iopen.net/projects/groupserver/
+
+.. _GroupServer.org: http://groupserver.org/
+.. _Michael JasonSmith: http://groupserver.org/p/mpj17
+.. _Creative Commons Attribution-Share Alike 3.0 New Zealand License:
+   http://creativecommons.org/licenses/by-sa/3.0/nz/
+.. _GroupServer: http://groupserver.org/
+.. _OnlineGroups.net: http://onlinegroups.net/
+
+.. [#group] See <http://source.iopen.net/groupserver/gs.group.stats/>
---
+++ setup.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+import os
+from setuptools import setup, find_packages
+from version import get_version
+
+version = get_version()
+
+setup(name='gs.site.stats',
+    version=version,
+    description="Usage statistics for a GroupServer site.",
+    long_description=open("README.txt").read() + "\n" +
+                      open(os.path.join("docs", "HISTORY.txt")).read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: Zope Public License',
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+    keywords='site groupserver stastics',
+    author='Michael JasonSmith',
+    author_email='mpj17@onlinegroups.net',
+    url='http://groupserver.org/',
+    license='ZPL 2.1',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['gs', 'gs.site', ],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'setuptools',
+        'zope.cachedescriptors',
+        'zope.component',
+        'gs.content.base',
+        'gs.content.layout',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,)
