1a0200dc44a85714c4b11d97e500cf9213a06848
Michael JasonSmith <mpj17@onlinegroups.net>
2016-02-29 11:03:47 -0800

    Adding to the unit tests

--- gs/site/stats/tests/posting.py
+++ gs/site/stats/tests/posting.py
@@ -13,6 +13,7 @@
 #
 ############################################################################
 from __future__ import absolute_import, unicode_literals, print_function
+from datetime import date
 from mock import (MagicMock, patch, PropertyMock)
 from unittest import TestCase
 from gs.site.stats.posting import (SitePostingStats, )
@@ -20,6 +21,19 @@ from gs.site.stats.posting import (SitePostingStats, )
 
 class TestSitePostingStats(TestCase):
     '''Test the ``SitePostingStats`` class'''
+
+    @patch.object(SitePostingStats, 'query', new_callable=PropertyMock)
+    def test_postStats_sorted(self, m_q):
+        'Test that the post-stats are sorted'
+        m_q().posts_per_day_on_site.return_value = [
+            self.post_stat(2, date(2016, 2, 29)), self.post_stat(1, date(2016, 2, 28)),
+            self.post_stat(3, date(2016, 3, 1)), ]
+        s = SitePostingStats(MagicMock(), MagicMock())
+        r = s.postStats
+
+        self.assertEqual(3, len(r))
+        self.assertEqual(1, r[0]['n_posts'])
+        self.assertEqual(3, r[-1]['n_posts'])
 
     @patch.object(SitePostingStats, 'postStats', new_callable=PropertyMock)
     def test_no_posts(self, m_pS):
@@ -29,3 +43,86 @@ class TestSitePostingStats(TestCase):
         r = s.postsExist
 
         self.assertFalse(r)
+
+    @patch.object(SitePostingStats, 'postStats', new_callable=PropertyMock)
+    def test_posts(self, m_pS):
+        'Test that the postsExist property returns True if there are posts'
+        m_pS.return_value = ['a', 'post', ]
+        s = SitePostingStats(MagicMock(), MagicMock())
+        r = s.postsExist
+
+        self.assertTrue(r)
+
+    @patch.object(SitePostingStats, 'postStats', new_callable=PropertyMock)
+    def test_min_no_posts(self, m_pS):
+        'Test that the minPerDay property returns 0 if there are no posts'
+        m_pS.return_value = []
+        s = SitePostingStats(MagicMock(), MagicMock())
+        r = s.minPerDay
+
+        self.assertEqual(0.0, r)
+
+    @staticmethod
+    def post_stat(n=1, date=date.today()):
+        retval = {'n_posts': n,
+                  'date': date, }
+        return retval
+
+    @patch.object(SitePostingStats, 'postStats', new_callable=PropertyMock)
+    def test_min_posts(self, m_pS):
+        'Test that the minPerDay property'
+        m_pS.return_value = [self.post_stat(2), self.post_stat(3), self.post_stat(1), ]
+        s = SitePostingStats(MagicMock(), MagicMock())
+        r = s.minPerDay
+
+        self.assertEqual(1, r)
+
+    @patch.object(SitePostingStats, 'postStats', new_callable=PropertyMock)
+    def test_max_no_posts(self, m_pS):
+        'Test that the maxPerDay property if there are no posts'
+        m_pS.return_value = []
+        s = SitePostingStats(MagicMock(), MagicMock())
+        r = s.maxPerDay
+
+        self.assertEqual(0, r)
+
+    @patch.object(SitePostingStats, 'postStats', new_callable=PropertyMock)
+    def test_max_posts(self, m_pS):
+        'Test that the minPerDay property'
+        m_pS.return_value = [self.post_stat(2), self.post_stat(3), self.post_stat(1), ]
+        s = SitePostingStats(MagicMock(), MagicMock())
+        r = s.maxPerDay
+
+        self.assertEqual(3, r)
+
+    @patch.object(SitePostingStats, 'postStats', new_callable=PropertyMock)
+    def test_mean_posts(self, m_pS):
+        'Test that the meanPerDay property'
+        m_pS.return_value = [
+            self.post_stat(1, date(2016, 2, 28)),
+            self.post_stat(2, date(2016, 2, 29)),
+            self.post_stat(3, date(2016, 3, 1)), ]
+
+        s = SitePostingStats(MagicMock(), MagicMock())
+        r = s.meanPerDay
+
+        self.assertEqual(3.0, r)
+
+    @patch.object(SitePostingStats, 'postStats', new_callable=PropertyMock)
+    def test_mean_posts_none(self, m_pS):
+        'Test that the meanPerDay property if there are no posts'
+        m_pS.return_value = []
+
+        s = SitePostingStats(MagicMock(), MagicMock())
+        with self.assertRaises(IndexError):  # --=mpj17=-- Do better
+            s.meanPerDay
+
+    @patch.object(SitePostingStats, 'postStats', new_callable=PropertyMock)
+    def test_mean_posts_one(self, m_pS):
+        'Test that the meanPerDay property returns 0 if there is just one day of posting'
+        m_pS.return_value = [self.post_stat(2, date(2016, 2, 29)), ]
+
+        s = SitePostingStats(MagicMock(), MagicMock())
+        r = s.meanPerDay
+
+        self.assertEqual(0.0, r)
