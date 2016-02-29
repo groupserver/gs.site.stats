d17931930c614d513f863c9b6180ce6c1ddf19a8
Michael JasonSmith <mpj17@onlinegroups.net>
2016-02-29 10:06:33 -0800

    Updating the product metadata

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ import os
 from setuptools import setup, find_packages
 from version import get_version
 
+name = 'gs.site.stats'
 version = get_version()
 
 with codecs.open('README.rst', encoding='utf-8') as f:
@@ -27,7 +28,7 @@ with codecs.open(os.path.join("docs", "HISTORY.rst"),
 
 
 setup(
-    name='gs.site.stats',
+    name=name,
     version=version,
     description="Usage statistics for a GroupServer site.",
     long_description=long_description,
@@ -46,10 +47,11 @@ setup(
     keywords='site, groupserver, stastics, stats',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
-    url='https://github.com/groupserver/gs.site.stats',
+    url='https://github.com/groupserver/{0}'.format(name),
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.site', ],
+    namespace_packages=['.'.join(name.split('.')[:i])
+                        for i in range(1, len(name.split('.')))],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
